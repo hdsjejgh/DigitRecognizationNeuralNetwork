be9c49c8f66335dc5f1cf87cd149df6dd1b25c9f
hdsjejgh <aayanahmad08@hotmail.com>
2025-01-07 13:47:41 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from typing import final
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+"""
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128,activation='relu'))
+model.add(tf.keras.layers.Dense(128,activation='relu'))
+model.add(tf.keras.layers.Dense(10,activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=40)
+
+model.save('mnist.keras')
+
+"""
+model = tf.keras.models.load_model('mnist.keras')
+
+image_number = 1
+while os.path.isfile(f"samples/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"samples/digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img])) #makes the images white on black
+        prediction = model.predict(img)
+        print(f"The Number is probably a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("error")
+    finally:
+        image_number+=1
